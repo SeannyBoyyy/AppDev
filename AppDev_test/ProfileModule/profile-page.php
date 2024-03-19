121f2261c296ab05f168eca735126b86b3806ac8
SeannyBoyyy <160821892+SeannyBoyyy@users.noreply.github.com>
2024-03-20 00:11:42 +0800

    add product

--- AppDev_test/ProfileModule/profile-page.php
+++ AppDev_test/ProfileModule/profile-page.php
@@ -11,7 +11,7 @@ if(isset($_SESSION['ownerID'])){
 
     $getSql = "SELECT name, text, image FROM business_profile WHERE owner = ?";
     $stmt = mysqli_prepare($conn, $getSql);
-    mysqli_stmt_bind_param($stmt, "i",$business_owner);
+    mysqli_stmt_bind_param($stmt, "i", $business_owner);
     mysqli_stmt_execute($stmt);
 
     $result = mysqli_stmt_get_result($stmt);
@@ -78,6 +78,44 @@ if(isset($_POST['submit'])) {
 
 }
 
+// add post
+if (isset($_POST["upload_product"])) {
+  $name_product = mysqli_real_escape_string($conn, $_POST["name_product"]);
+  $text_product = mysqli_real_escape_string($conn, $_POST["text_product"]);
+
+    if ($_FILES["image_product"]["error"] === 4) {
+        echo "<script> alert('Image does not exist'); </script>";
+    } else {
+        $fileName = $_FILES["image_product"]["name"];
+        $fileSize = $_FILES["image_product"]["size"];
+        $tmpName = $_FILES["image_product"]["tmp_name"];
+
+        $validImageExtension = ['jpg', 'jpeg', 'png'];
+        $imageExtension = pathinfo($fileName, PATHINFO_EXTENSION);
+
+        if (!in_array($imageExtension, $validImageExtension)) {
+            echo "<script> alert('Invalid image extension'); </script>";
+        } elseif ($fileSize > 1000000) {
+            echo "<script> alert('Image size is too large'); </script>";
+        } else {
+            $newImageName = uniqid() . '.' . $imageExtension;
+            $uploadPath = './img/' . $newImageName;
+        
+            move_uploaded_file($tmpName, $uploadPath);
+
+            $text_product = mysqli_real_escape_string($conn, $_POST["text_product"]);
+
+            $query = "INSERT INTO posting_module (name, text, image, posted_by) VALUES ('$name_product', '$newImageName', '$text_product', $business_owner)";
+            mysqli_query($conn, $query);
+
+            echo "<script> 
+                    alert('Image uploaded successfully'); 
+                    document.location.href = 'profile-page.php';
+                  </script>";
+        }
+    }
+}
+
 ?>
 
 
@@ -107,7 +145,7 @@ if(isset($_POST['submit'])) {
             <div class="text-center">
                 <div class="nav flex-column nav-pills me-3 mt-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                     <button class="nav-link" type="button"><a href="../index.php" class="logoutBTN btn">Home</a></button>
-                    <button class="nav-link active"  id="v-pills-upload-tab" data-bs-toggle="pill" data-bs-target="#v-pills-upload" type="button" role="tab" aria-controls="v-pills-upload" aria-selected="true">Upload a new post</button>
+                    <button class="nav-link active"  id="v-pills-upload-tab" data-bs-toggle="pill" data-bs-target="#v-pills-upload" type="button" role="tab" aria-controls="v-pills-upload" aria-selected="true">Upload a new Product</button>
                     <button class="nav-link" id="v-pills-managePost-tab" data-bs-toggle="pill" data-bs-target="#v-pills-managePost" type="button" role="tab" aria-controls="v-pills-managePost" aria-selected="false">Manage Posts</button>
                     <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Update Profile</button>
                     <button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</button>
@@ -121,22 +159,26 @@ if(isset($_POST['submit'])) {
                     <div class="container-fluid w-50" style="margin-top: 90px;">
                         <div class="col-md-6 container-fluid text-center">
                             <div class="container-fluid">
-                                <h1>Upload a new post</h1>
+                                <h1>Upload a new Product</h1>
                             </div>
                         </div>
                     </div>
                     <div class="container-sm d-flex align-items-center mt-5 w-50 border rounded-5 p-3 bg-white shadow box-area p-5">
-                        <form class="row w-100 g-3">
+                        <form class="row w-100 g-3" action="" method="post" enctype="multipart/form-data">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="floatingInput name_product" placeholder="" name="name_product" value="">
+                                <label for="floatingInput" style="margin-left: 5px;">Product Name</label>
+                            </div>
                             <div class="col-12">
                                 <label for="formFile" class="form-label" style="margin-left: 5px;">Upload a picture</label>
-                                <input class="form-control" type="file" id="formFile">
+                                <input class="form-control" type="file" id="formFile" name="image_product">
                             </div>
                             <div class="mb-3">
-                                <label for="exampleFormControlTextarea1" class="form-label">Write something about the post.</label>
-                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                <label for="exampleFormControlTextarea1" class="form-label">Write something about the Product.</label>
+                                <textarea class="form-control" id="exampleFormControlTextarea1 text_product" rows="3" name="text_product"></textarea>
                             </div>
                             <div class="col-12">
-                                <button class="btn btn-lg fs-6 w-100" type="submit" style="background-color: #90EE90;">Upload</button>
+                                <button class="btn btn-lg fs-6 w-100" type="upload_product" name="upload_product" style="background-color: #90EE90;">Upload</button>
                             </div>
                         </form>
                     </div>
