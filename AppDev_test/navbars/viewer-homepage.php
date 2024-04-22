731ff2bb24b124b8fe10bd28dad68c2ae81a77ab
AndyLuuuu <andylu0626@yahoo.com.tw>
2024-04-23 01:57:26 +0800

    login navbar icon
    icon

--- AppDev_test/navbars/viewer-homepage.php
+++ AppDev_test/navbars/viewer-homepage.php
@@ -9,6 +9,13 @@
     <link rel="stylesheet" href="../CSS/view-farm.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <style>
+      .nav-link:hover{
+        color:black;
+        transition: color 0.3s ease, transform 0.3s ease;
+        transform: translateY(-5px);
+      }
+      </style>
   </head>
   <body>
   <nav class="navbar navbar sticky-top p-3" style="background-color: white;">
@@ -19,10 +26,10 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <div class="navbar-nav ">
-                <a class="nav-link" aria-current="page" href="#farms"><i class="fas fa-tractor"></i>Farm</a>
-                <a class="nav-link" aria-current="page" href="#"><i class="fas fa-shopping-basket"></i>Products</a>
-                <a class="nav-link" aria-current="page" href="./AccPages/login-page.php"><i class="fas fa-sign-in-alt"></i>Log In</a>
-                <a class="nav-link" aria-current="page" href="./AccPages/register-page.php"><i class="fas fa-user"></i>Register</a>
+                <a class="nav-link" aria-current="page" href="#farms"><i class="fas fa-tractor" style="padding:10px;"></i>Farm</a>
+                <a class="nav-link" aria-current="page" href="#"><i class="fas fa-shopping-basket" style="padding:10px;"></i>Products</a>
+                <a class="nav-link" aria-current="page" href="./AccPages/login-page.php"><i class="fas fa-sign-in-alt" style="padding:10px;"></i>Log In</a>
+                <a class="nav-link" aria-current="page" href="./AccPages/register-page.php"><i class="fas fa-user" style="padding:10px;"></i>Register</a>
             </div>
         </div>
     </div>
