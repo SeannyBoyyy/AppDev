c51418d3482868fe267fed7bc3c7c092b0942331
SeannyBoyyy <160821892+SeannyBoyyy@users.noreply.github.com>
2024-04-25 11:25:49 +0800

    about-us container / navbar-dropdown

--- AppDev_test/admin-landingPage.php
+++ AppDev_test/admin-landingPage.php
@@ -114,10 +114,10 @@
     </div>
     
 
-    <div class=" mt-5">
+    <div class="container mt-5">
 		<div class="row">
 			<div class="col">
-				<div class="card" style="background-color: #ECEFF1; width:1300px;margin-left:auto;margin-right:auto;">
+				<div class="card" style="background-color: #ECEFF1;">
 				    <div class="card-body">
                         <div class="row m-5">
                             <div class="col-12 p-6">
