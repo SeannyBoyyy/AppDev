fbfda4894e3d8e5124265d30a4f16a6b5204d145
SeannyBoyyy <160821892+SeannyBoyyy@users.noreply.github.com>
2024-05-08 02:46:20 +0800

    ratings and reviews

--- AppDev_test/admin-viewFarm.php
+++ AppDev_test/admin-viewFarm.php
@@ -144,6 +144,11 @@
 ?>
 <head>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
     <style>
         .card-body h5 {
@@ -289,6 +294,158 @@
         </div>
     </div>
 
+    <div class="row justify-contents-center align-items-center text-cnter">
+        <div class="container" style="max-width: 70%;">
+            <div class="card">
+                <div class="card-header"><h1 class="mt-3 mb-3">Review & Rating</h1></div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-4 text-center">
+                            <h1 class="text-warning mt-4 mb-4">
+                                <b><span id="average_rating">0.0</span> / 5</b>
+                            </h1>
+                            <div class="mb-3">
+                                <i class="fas fa-star star-light mr-1 main_star"></i>
+                                <i class="fas fa-star star-light mr-1 main_star"></i>
+                                <i class="fas fa-star star-light mr-1 main_star"></i>
+                                <i class="fas fa-star star-light mr-1 main_star"></i>
+                                <i class="fas fa-star star-light mr-1 main_star"></i>
+                            </div>
+                            <h3><span id="total_review">0</span> Review</h3>
+                        </div>
+                        <div class="col-sm-4">
+                            <p>
+                                <div class="progress-label-left"><b>5</b> <i class="fas fa-star text-warning"></i></div>
+
+                                <div class="progress-label-right">(<span id="total_five_star_review">0</span>)</div>
+                                <div class="progress">
+                                    <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="five_star_progress"></div>
+                                </div>
+                            </p>
+                            <p>
+                                <div class="progress-label-left"><b>4</b> <i class="fas fa-star text-warning"></i></div>
+                                
+                                <div class="progress-label-right">(<span id="total_four_star_review">0</span>)</div>
+                                <div class="progress">
+                                    <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="four_star_progress"></div>
+                                </div>               
+                            </p>
+                            <p>
+                                <div class="progress-label-left"><b>3</b> <i class="fas fa-star text-warning"></i></div>
+                                
+                                <div class="progress-label-right">(<span id="total_three_star_review">0</span>)</div>
+                                <div class="progress">
+                                    <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="three_star_progress"></div>
+                                </div>               
+                            </p>
+                            <p>
+                                <div class="progress-label-left"><b>2</b> <i class="fas fa-star text-warning"></i></div>
+                                
+                                <div class="progress-label-right">(<span id="total_two_star_review">0</span>)</div>
+                                <div class="progress">
+                                    <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="two_star_progress"></div>
+                                </div>               
+                            </p>
+                            <p>
+                                <div class="progress-label-left"><b>1</b> <i class="fas fa-star text-warning"></i></div>
+                                
+                                <div class="progress-label-right">(<span id="total_one_star_review">0</span>)</div>
+                                <div class="progress">
+                                    <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="one_star_progress"></div>
+                                </div>               
+                            </p>
+                        </div>
+                        <div class="col-sm-4 text-center">
+                            <h3 class="mt-4 mb-3">Write Review Here</h3>
+                            <button type="button" name="add_review" id="add_review" class="btn btn-primary">Review</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mt-5" id="review_content">
+            <?php
+            // Query to fetch reviews based on business_id
+            $query = "SELECT * FROM review_table WHERE business_id = '$business_id' ORDER BY review_id DESC";
+            $result = mysqli_query($conn, $query);
+
+            // Check if there are any reviews
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    // Convert timestamp to human-readable date
+                    $review_date = date("l jS, F Y h:i:s A", $row['datetime']);
+            ?>
+                    <div class="row mb-3">
+                        <div class="col-sm-1">
+                            <div class="rounded-circle bg-danger text-white pt-2 pb-2">
+                                <h3 class="text-center"><?php echo strtoupper(substr($row['user_name'], 0, 1)); ?></h3>
+                            </div>
+                        </div>
+                        <div class="col-sm-11">
+                            <div class="card">
+                                <div class="card-header"><b><?php echo $row['user_name']; ?></b></div>
+                                <div class="card-body">
+                                    <?php
+                                    // Display star ratings
+                                    for ($star = 1; $star <= 5; $star++) {
+                                        $class_name = ($row['user_rating'] >= $star) ? 'text-warning' : 'star-light';
+                                    ?>
+                                        <i class="fas fa-star <?php echo $class_name; ?> mr-1"></i>
+                                    <?php } ?>
+                                    <br />
+                                    <?php echo $row['user_review']; ?>
+                                </div>
+                                <div class="card-footer text-right">On <?php echo $review_date; ?></div>
+                            </div>
+                        </div>
+                    </div>
+            <?php
+                }
+            } else {
+                // No reviews found
+                echo "<p>No reviews found.</p>";
+            }
+            ?>
+
+
+            </div>
+        </div>
+
+        <div id="review_modal" class="modal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Submit Review</h5>
+                        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <h4 class="text-center mt-2 mb-4">
+                            <i class="fas fa-star star-light submit_star mr-1" id="submit_star_1" data-rating="1"></i>
+                            <i class="fas fa-star star-light submit_star mr-1" id="submit_star_2" data-rating="2"></i>
+                            <i class="fas fa-star star-light submit_star mr-1" id="submit_star_3" data-rating="3"></i>
+                            <i class="fas fa-star star-light submit_star mr-1" id="submit_star_4" data-rating="4"></i>
+                            <i class="fas fa-star star-light submit_star mr-1" id="submit_star_5" data-rating="5"></i>
+                        </h4>
+                        <div class="form-group">
+                            <input type="text" name="user_name" id="user_name" class="form-control" placeholder="Enter Your Name" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="busines_id" id="business_id" class="form-control" hidden />
+                        </div>
+                        <div class="form-group">
+                            <textarea name="user_review" id="user_review" class="form-control" placeholder="Type Review Here"></textarea>
+                        </div>
+                        <div class="form-group text-center mt-4">
+                            <button type="button" class="btn btn-primary" id="save_review">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+                    
+    </div>
+
     <div class="sticky-bottom">
         <button type="button"  class="btn btn-lg bg-white position-absolute bottom-0 end-0" data-bs-toggle="modal" data-bs-target="#exampleModal">
             <small>Message us!</small>
@@ -340,6 +497,178 @@
 </div>
 
 
+<style>
+.progress-label-left
+{
+    float: left;
+    margin-right: 0.5em;
+    line-height: 1em;
+}
+.progress-label-right
+{
+    float: right;
+    margin-left: 0.3em;
+    line-height: 1em;
+}
+.star-light
+{
+	color:#e9ecef;
+}
+</style>
 
+<script>
 
+$(document).ready(function(){
 
+	var rating_data = 0;
+
+    $('#add_review').click(function(){
+
+        $('#review_modal').modal('show');
+
+    });
+
+    $(document).on('mouseenter', '.submit_star', function(){
+
+        var rating = $(this).data('rating');
+
+        reset_background();
+
+        for(var count = 1; count <= rating; count++)
+        {
+
+            $('#submit_star_'+count).addClass('text-warning');
+
+        }
+
+    });
+
+    function reset_background()
+    {
+        for(var count = 1; count <= 5; count++)
+        {
+
+            $('#submit_star_'+count).addClass('star-light');
+
+            $('#submit_star_'+count).removeClass('text-warning');
+
+        }
+    }
+
+    $(document).on('mouseleave', '.submit_star', function(){
+
+        reset_background();
+
+        for(var count = 1; count <= rating_data; count++)
+        {
+
+            $('#submit_star_'+count).removeClass('star-light');
+
+            $('#submit_star_'+count).addClass('text-warning');
+        }
+
+    });
+
+    $(document).on('click', '.submit_star', function(){
+
+        rating_data = $(this).data('rating');
+
+    });
+
+    $('#save_review').click(function(){
+
+        var user_name = $('#user_name').val();
+        var business_id = "<?php echo $business_id; ?>";
+        var user_review = $('#user_review').val();
+
+        if(user_name == '' || user_review == '')
+        {
+            Swal.fire({
+                title: 'Error!',
+                text: 'Please fill in all the fields.',
+                icon: 'error'
+            });
+            return false;
+        }
+        else
+        {
+            $.ajax({
+                url:"submit_rating.php?business_id=<?php echo $business_id;?>",
+                method:"POST",
+                data:{rating_data:rating_data, user_name:user_name, business_id:business_id, user_review:user_review},
+                success:function(data)
+                {
+                    $('#review_modal').modal('hide');
+
+                    load_rating_data();
+
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Your Review & Rating Successfully Submitted!',
+                        icon: 'success'
+                    }).then(function() {
+                        window.location = 'admin-viewFarm.php?business_id=<?php echo $business_id;?>';
+                    });
+                }
+            })
+        }
+
+    });
+
+    load_rating_data();
+
+    function load_rating_data()
+    {
+        $.ajax({
+            url:"submit_rating.php?business_id=<?php echo $business_id;?>",
+            method:"POST",
+            data:{action:'load_data', business_id: '<?php echo $business_id; ?>'},
+            dataType:"JSON",
+            success:function(data)  
+            {
+                $('#average_rating').text(data.average_rating);
+                $('#total_review').text(data.total_review);
+
+                var count_star = 0;
+
+                $('.main_star').each(function(){
+                    count_star++;
+                    if(Math.ceil(data.average_rating) >= count_star)
+                    {
+                        $(this).addClass('text-warning');
+                        $(this).addClass('star-light');
+                    }
+                });
+
+                $('#total_five_star_review').text(data.five_star_review);
+
+                $('#total_four_star_review').text(data.four_star_review);
+
+                $('#total_three_star_review').text(data.three_star_review);
+
+                $('#total_two_star_review').text(data.two_star_review);
+
+                $('#total_one_star_review').text(data.one_star_review);
+
+                $('#five_star_progress').css('width', (data.five_star_review/data.total_review) * 100 + '%');
+
+                $('#four_star_progress').css('width', (data.four_star_review/data.total_review) * 100 + '%');
+
+                $('#three_star_progress').css('width', (data.three_star_review/data.total_review) * 100 + '%');
+
+                $('#two_star_progress').css('width', (data.two_star_review/data.total_review) * 100 + '%');
+
+                $('#one_star_progress').css('width', (data.one_star_review/data.total_review) * 100 + '%');
+
+            }
+        })
+    }
+
+});
+
+</script>
+
+<?php
+    include('./navbars/viewer-footer.php');
+    include('./navbars/footer.php');
+?>
